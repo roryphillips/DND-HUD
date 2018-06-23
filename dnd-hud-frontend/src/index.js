b064f7365321b53ee9a5fc8634186ec23c41bdb8
Rory Phillips <roryphillipsdev@gmail.com>
2018-06-23 00:37:32 +0000

    Created the framework for a basic client->server socket communication

--- dnd-hud-frontend/src/index.js
+++ dnd-hud-frontend/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import SocketIOShell from "./containers/SocketIOShell";
+import configureStore from './store/configureStore';
+
+const store = configureStore({});
+
+ReactDOM.render(
+    <Provider store={store}>
+        <SocketIOShell>
+            <App />
+        </SocketIOShell>
+    </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
