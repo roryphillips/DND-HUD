6dc03c2d67a78a9352ef9b8850ed99ba0ffc39fa
Rory Phillips <roryphillipsdev@gmail.com>
2018-06-23 12:27:39 +1000

    Events are being further grouped to whatever store section they are relevant to

--- dnd-hud-backend/events/mapEvents.js
+++ dnd-hud-backend/events/mapEvents.js
@@ -1,9 +1,5 @@
-const registerAddCharacter = require('./addCharacter');
-const registerDamageCharacter = require('./damageCharacter');
-const registerHealCharacter = require('./healCharacter');
-const registerAddCharacterCondition = require('./addCondition');
-const registerRemoveCharacterCondition = require ('./removeCondition');
-const registerSetInitiative = require('./setInitiative');
+const registerCharacterEvents = require('./characters/registerCharacterEvents');
+const registerInitiativeEvents = require('./initiative/registerInitativeEvents');
 
 module.exports = (socket, store) => {
     console.log(`New Connection: ${socket.id}`);
@@ -17,10 +13,6 @@ module.exports = (socket, store) => {
         console.log(JSON.stringify(data));
     });
 
-    registerAddCharacter(socket, store.characters || {});
-    registerDamageCharacter(socket, store.characters || {});
-    registerHealCharacter(socket, store.characters || {});
-    registerAddCharacterCondition(socket, store.characters || {});
-    registerRemoveCharacterCondition(socket, store.characters || {});
-    registerSetInitiative(socket, store.initiative || {});
+    registerCharacterEvents(socket, store.characters || {});
+    registerInitiativeEvents(socket, store.initiative || {});
 };
